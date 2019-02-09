83be58afa4b2a89e211c3c0b765efc5afcbd0bc7
Egon Elbre <egonelbre@gmail.com>
2019-02-09 09:29:25 +0200

    Optimize flatten SQL

--- sqlgen/common.go
+++ sqlgen/common.go
@@ -15,7 +15,6 @@
 package sqlgen
 
 import (
-	"regexp"
 	"strings"
 )
 
@@ -60,8 +59,52 @@ func Render(dialect Dialect, sql SQL, ops ...RenderOp) string {
 	return dialect.Rebind(out)
 }
 
-var reSpace = regexp.MustCompile(`\s+`)
+func flattenSQL(x string) string {
+	// trim whitespace from beginning and end
+	s, e := 0, len(x)-1
+	for s < len(x) && (x[s] == ' ' || x[s] == '\t' || x[s] == '\n') {
+		s++
+	}
+	for s <= e && (x[e] == ' ' || x[e] == '\t' || x[e] == '\n') {
+		e--
+	}
+	if s > e {
+		return ""
+	}
+	x = x[s : e+1]
 
-func flattenSQL(s string) string {
-	return strings.TrimSpace(reSpace.ReplaceAllString(s, " "))
+	// check for whitespace that needs fixing
+	wasSpace := false
+	for i := 0; i < len(x); i++ {
+		r := x[i]
+		justSpace := r == ' '
+		if (wasSpace && justSpace) || r == '\t' || r == '\n' {
+			// whitespace detected, start writing a new string
+			var result strings.Builder
+			result.Grow(len(x))
+			if wasSpace {
+				result.WriteString(x[:i-1])
+			} else {
+				result.WriteString(x[:i])
+			}
+			for p := i; p < len(x); p++ {
+				for p < len(x) && (x[p] == ' ' || x[p] == '\t' || x[p] == '\n') {
+					p++
+				}
+				result.WriteByte(' ')
+
+				start := p
+				for p < len(x) && !(x[p] == ' ' || x[p] == '\t' || x[p] == '\n') {
+					p++
+				}
+				result.WriteString(x[start:p])
+			}
+
+			return result.String()
+		}
+		wasSpace = justSpace
+	}
+
+	// no problematic whitespace found
+	return x
 }
